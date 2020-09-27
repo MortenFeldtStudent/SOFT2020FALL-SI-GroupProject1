c60c58132ff2baec893563548324ea34721582d5
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-09-27 13:42:58 +0200

    Project files
    Project files from start of project.

---
+++ src/main/java/dk/si/groupproject1/Groupproject1Application.java
@@ -0,0 +1,13 @@
+package dk.si.groupproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Groupproject1Application.class, args);
+    }
+
+}
